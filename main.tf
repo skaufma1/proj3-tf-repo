e8e0290bf392b97fab99060312db1b5b5bff3b08
Shmuel Kaufmann <shmuel.kaufmann1@gmail.com>
2023-06-29 17:58:40 +0300

    Adding main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "ec2" {
+    ami = "ami-0261755bbcb8c4a84"
+    instance_type = "t2.micro"
+    tags = {
+        Name = "Terraform Test ABC"
+    }
+}
