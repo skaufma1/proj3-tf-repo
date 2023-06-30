a86f3f9972aeb3fbcf69d5b88786bf2bde836980
Shmuel Kaufmann <shmuel.kaufmann1@gmail.com>
2023-06-30 18:27:56 +0300

    Jenkinsfile corr

--- terraform_project/modules/prod1/main.tf
+++ terraform_project/modules/prod1/main.tf
@@ -6,6 +6,6 @@ resource "aws_instance" "ec2" {
     ami = "ami-0261755bbcb8c4a84"
     instance_type = "t2.small"
     tags = {
-        Name = "TF-Slave-Test"
+        Name = "TF-Prod1-Test"
     }
 }
