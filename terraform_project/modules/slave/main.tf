1cb41dc97ca2a805ffa56faeb48f68636b2eebc0
Shmuel Kaufmann <shmuel.kaufmann1@gmail.com>
2023-07-01 11:58:40 +0300

    Jenkinsfile corr

--- terraform_project/modules/slave/main.tf
+++ terraform_project/modules/slave/main.tf
@@ -9,3 +9,14 @@ resource "aws_instance" "ec2" {
         Name = "TF-Slave-Test"
     }
 }
+
+# Create elastic ip
+resource "aws_eip" "lb" {
+    vpc = true
+}
+
+# connecting between ec2 instance and elastic ip instance
+resource "aws_eip_association" "eip_assoc" {
+    instance_id = aws_instance.ec2.id
+    allocation_id = aws_eip.lb.id
+}
