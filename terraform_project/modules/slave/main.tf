467478c260d7dd5c69f7677dd261f2eb4f6e7db8
Shmuel Kaufmann <shmuel.kaufmann1@gmail.com>
2023-07-02 11:25:51 +0300

    main.tf changes for Module: Slave

--- terraform_project/modules/slave/main.tf
+++ terraform_project/modules/slave/main.tf
@@ -25,7 +25,7 @@ resource "aws_instance" "ec2" {
 
     # Docker installation
     provisioner "local-exec" {
-        command = "curl ifconfig.co"
+        command = "whoami"
         # command = <<-EOT
         #     sudo apt-get update
         # EOT
