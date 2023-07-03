c0b28972b7d32d30f20a3160f4eedb1391413dfe
Shmuel Kaufmann <shmuel.kaufmann1@gmail.com>
2023-07-03 18:47:44 +0300

    main.tf added for Load Balancer

--- terraform_project/modules/load_balancer/main.tf
+++ terraform_project/modules/load_balancer/main.tf
@@ -105,15 +105,17 @@ resource "aws_lb" "load_balancer" {
         "sg-0be101c0f47493d2e"
     ]
 
-    listener {
-        port     = 80
-        protocol = "HTTP"
+    listeners = [
+        {
+            port     = 80
+            protocol = "HTTP"
 
-        default_action {
-            type             = "forward"
-            target_group_arn = aws_lb_target_group.target_group.arn
+            default_action {
+                type             = "forward"
+                target_group_arn = aws_lb_target_group.target_group.arn
+            }
         }
-    }
+    ]
 }
 
 resource "aws_lb_target_group" "target_group" {
