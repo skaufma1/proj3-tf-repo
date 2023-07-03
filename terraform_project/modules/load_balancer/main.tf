180c9b2b46cc3b3fb689a3b556d33ee88f009330
Shmuel Kaufmann <shmuel.kaufmann1@gmail.com>
2023-07-04 00:17:53 +0300

    main.tf added for Load Balancer

--- terraform_project/modules/load_balancer/main.tf
+++ terraform_project/modules/load_balancer/main.tf
@@ -180,80 +180,92 @@
 # }
 
 provider "aws" {
-  region = "us-east-1"
+    region = "us-east-1"
 }
 
 data "aws_instances" "ec2_prod1_instance" {
-  filter {
-    name   = "tag:Name"
-    values = ["proj3-tf-prod1"]
-  }
+    filter {
+        name   = "tag:Name"
+        values = ["proj3-tf-prod1"]
+    }
 }
 
 data "aws_instances" "ec2_prod2_instance" {
-  filter {
-    name   = "tag:Name"
-    values = ["proj3-tf-prod2"]
-  }
+    filter {
+        name   = "tag:Name"
+        values = ["proj3-tf-prod2"]
+    }
 }
 
 resource "aws_lb_target_group" "tg" {
-  name     = "my-target-group"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = "vpc-081229f33440b91ea"
+    name     = "my-target-group"
+    port     = 80
+    protocol = "HTTP"
+    vpc_id   = "vpc-081229f33440b91ea"
 }
 
 resource "aws_lb_target_group_attachment" "attach_prod1" {
-  target_group_arn = aws_lb_target_group.tg.arn
-  target_id        = data.aws_instances.ec2_prod1_instance.ids[0]
+    target_group_arn = aws_lb_target_group.tg.arn
+    target_id        = data.aws_instances.ec2_prod1_instance.ids[0]
 }
 
 resource "aws_lb_target_group_attachment" "attach_prod2" {
-  target_group_arn = aws_lb_target_group.tg.arn
-  target_id        = data.aws_instances.ec2_prod2_instance.ids[0]
+    target_group_arn = aws_lb_target_group.tg.arn
+    target_id        = data.aws_instances.ec2_prod2_instance.ids[0]
 }
 
 resource "aws_lb_listener" "listener" {
-  load_balancer_arn = aws_lb.load_balancer.arn
-  port              = 80
-  protocol          = "HTTP"
+    load_balancer_arn = aws_lb.load_balancer.arn
+    port              = 80
+    protocol          = "HTTP"
 
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.tg.arn
-  }
+    default_action {
+        type             = "forward"
+        target_group_arn = aws_lb_target_group.tg.arn
+    }
 }
 
 resource "aws_lb" "load_balancer" {
-  name             = "my-load-balancer"
-  internal         = false
-  load_balancer_type = "application"
+    name             = "my-load-balancer"
+    internal         = false
+    load_balancer_type = "application"
 
-  subnets = [
-    "subnet-0fc1eecf0a8d5fe36",
-    "subnet-0a01c96aef97d56bd",
-    "subnet-0ca600ed19f9d5a48",
-    "subnet-04db0ab5196e2f002",
-    "subnet-0f30605e99b332f16",
-    "subnet-07a42efc118a52b57"
-  ]
+    subnets = [
+        "subnet-0fc1eecf0a8d5fe36",
+        "subnet-0a01c96aef97d56bd",
+        "subnet-0ca600ed19f9d5a48",
+        "subnet-04db0ab5196e2f002",
+        "subnet-0f30605e99b332f16",
+        "subnet-07a42efc118a52b57"
+    ]
 
-  security_groups = [
-    "sg-00102fd2ef88fd054",
-    "sg-0be101c0f47493d2e"
-  ]
+    security_groups = [
+        "sg-00102fd2ef88fd054",
+        "sg-0be101c0f47493d2e"
+    ]
 }
 
 resource "aws_lb_listener_rule" "tg_rule" {
-  listener_arn = aws_lb_listener.listener.arn
+    listener_arn = aws_lb_listener.listener.arn
 
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.tg.arn
-  }
+    action {
+        type             = "forward"
+        target_group_arn = aws_lb_target_group.tg.arn
+    }
 
-  condition {
-    # No specific condition check
-  }
+    condition {
+        # No specific condition check
+    }
 }
+
+resource "aws_lb_target_group_health_check" "tg_health_check" {
+    target_group_arn = aws_lb_target_group.tg.arn
+    interval         = 30
+    timeout          = 10
+    healthy_threshold = 3
+    unhealthy_threshold = 3
+    path             = "/"
+    port             = 80
+    protocol         = "HTTP"
+}
+
