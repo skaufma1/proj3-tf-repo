03d5f7caa8c623248f7e228f804fb794de860557
Shmuel Kaufmann <shmuel.kaufmann1@gmail.com>
2023-07-04 12:27:55 +0300

    Module: lb. Health Check params

--- terraform_project/modules/load_balancer/main.tf
+++ terraform_project/modules/load_balancer/main.tf
@@ -18,7 +18,7 @@ data "aws_instances" "ec2_prod2_instance" {
 
 resource "aws_lb_target_group" "tg" {
     name     = "proj3-tf-lb-target-group"
-    port     = 5000
+    port     = 80
     protocol = "HTTP"
     vpc_id   = "vpc-081229f33440b91ea"
 
